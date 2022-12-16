6270ea9af3a2978e49df035e7eaa8cb76c85f57e
alexlieb <alexliebhaber1992@gmail.com>
2022-12-16 12:24:28 -0600

    finished app

--- script.js
+++ script.js
@@ -2,62 +2,108 @@ document.getElementById("issueInputForm").addEventListener("submit", saveIssue);
 
 function fetchIssues() {
   let issues = JSON.parse(localStorage.getItem("issues"));
-  // this is our div from HTML
   let issuesList = document.getElementById("issuesList");
+  console.log(issues);
 
   issuesList.innerHTML = "";
 
-  for (let i = 0; i < issuesList.length; i++) {
-    // grabbing issues info from form
+  for (let i = 0; i < issues.length; i++) {
     let id = issues[i].id;
     let subject = issues[i].subject;
     let description = issues[i].description;
     let severity = issues[i].severity;
     let assignedTo = issues[i].assignedTo;
     let status = issues[i].status;
-    // change colors depending on status
-    let statusColor = status === "Closed" ? "label-success" : "label-info";
+    let statusColor = status == "Closed" ? "label-success" : "label-info";
 
-    // issuesList.innerHTML +=
+    issuesList.innerHTML +=
+      '<div class="well">' +
+      "<h6>Issue ID:" +
+      id +
+      "</h6>" +
+      '<p><span class= "label ' +
+      statusColor +
+      ' ">' +
+      status +
+      "</span></p>" +
+      "<h3>" +
+      subject +
+      "</h3>" +
+      "<p>" +
+      description +
+      "</p>" +
+      '<p><span class="glyphicon glyphicon-time"></span> ' +
+      severity +
+      " " +
+      '<span class="glyphicon glyphicon-user"></span>' +
+      assignedTo +
+      "</p>" +
+      '<a href="#" class="btn btn-warning" onclick="setStatusClosed(\'' +
+      id +
+      "')\">Close</a> " +
+      '<a href="#" class="btn btn-danger" onclick="deleteIssue(\'' +
+      id +
+      "')\">Delete</a> " +
+      "</div>";
   }
 }
 
 function saveIssue(e) {
-  // generate unique id using chance.js
   let issueId = chance.guid();
-  let issueSubject = document.getElementById("issueSubjectInput").value;
-  let issueDescription = document.getElementById("issueDescriptionInput").value;
+  let issueSubject = document.getElementById("issueSubjInput").value;
+  let issueDesc = document.getElementById("issueDescInput").value;
   let issueSeverity = document.getElementById("issueSeverityInput").value;
   let issueAssignedTo = document.getElementById("issueAssignedToInput").value;
   let issueStatus = "Open";
 
-  // create object from form info
   let issue = {
     id: issueId,
     subject: issueSubject,
-    description: issueDescription,
+    description: issueDesc,
     severity: issueSeverity,
     assignedTo: issueAssignedTo,
     status: issueStatus,
   };
 
-  // store each issue object in array, and set in localStorage
   if (localStorage.getItem("issues") === null) {
     let issues = [];
     issues.push(issue);
     localStorage.setItem("issues", JSON.stringify(issues));
   } else {
-    // if issues already exists
-    // grab it, push new issue, the re-set in localStorage
     let issues = JSON.parse(localStorage.getItem("issues"));
     issues.push(issue);
     localStorage.setItem("issues", JSON.stringify(issues));
   }
 
-  //reset form after submission
   document.getElementById("issueInputForm").reset();
 
   fetchIssues();
 
   e.preventDefault();
+}
+
+function setStatusClosed(id) {
+  let issues = JSON.parse(localStorage.getItem("issues"));
+  for (let i = 0; i < issues.length; i++) {
+    if (issues[i].id === id) {
+      issues[i].status = "Closed";
+    }
+  }
+
+  localStorage.setItem("issues", JSON.stringify(issues));
+
+  fetchIssues();
+}
+
+function deleteIssue(id) {
+  let issues = JSON.parse(localStorage.getItem("issues"));
+  for (let i = 0; i < issues.length; i++) {
+    if (issues[i].id === id) {
+      issues.splice(i, 1);
+    }
+  }
+
+  localStorage.setItem("issues", JSON.stringify(issues));
+
+  fetchIssues();
 }
